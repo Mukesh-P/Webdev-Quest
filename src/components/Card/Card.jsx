02d2a52aa054025bec597ecfa8731b3b4cd5efae
Hsekumsti@gmail.com <Hsekumsti@gmail.com>
2026-06-03 11:39:29 +0530

    feat: complete day 3 card component, lol

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,21 @@
 import './Card.css';
 
-export const Card = () => {
-  return <div></div>;
+const CardHeader = ({ children }) => {
+  return <div className="card-header">{children}</div>;
 };
+
+const CardBody = ({ children }) => {
+  return <div className="card-body">{children}</div>;
+}
+
+const CardFooter = ({ children }) => {
+  return <div className="card-footer">{children}</div>;
+}
+
+export const Card = ({ children, ...rest }) => {
+  return <div className="card" {...rest}>{children}</div>;
+};
+
+Card.Header = CardHeader;
+Card.Body = CardBody;
+Card.Footer = CardFooter;

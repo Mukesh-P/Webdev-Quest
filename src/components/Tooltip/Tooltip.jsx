9c5a0e614477564a6ca18d2b4b5ed5eef5f7920f
Hsekumsti@gmail.com <Hsekumsti@gmail.com>
2026-06-09 21:37:24 +0530

    feat: complete Day 7 Tooltip component

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -1,5 +1,25 @@
+import { useState, cloneElement } from 'react';
 import './Tooltip.css';
 
-export const Tooltip = () => {
-  return <div></div>;
+export const Tooltip = ({ children, content }) => {
+  const [visible, setVisible] = useState(false);
+
+  const show = () => setVisible(true);
+  const hide = () => setVisible(false);
+
+  return (
+    <div className="tooltip-wrapper">
+      {cloneElement(children, {
+        onMouseEnter: show,
+        onMouseLeave: hide,
+        onFocus: show,
+        onBlur: hide,
+      })}
+      {visible && (
+        <span className="tooltip-content" role="tooltip">
+          {content}
+        </span>
+      )}
+    </div>
+  );
 };
